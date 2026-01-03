625ba223a6b07049a33322c97619a62eead106ba
Jason Y <110353075+JasonY199@users.noreply.github.com>
2026-01-02 19:21:41 -0500

    add logo image and favicon

--- components/layout/header.tsx
+++ components/layout/header.tsx
@@ -19,9 +19,13 @@ export function Header() {
         <Link
           href="/"
           onClick={() => setMobileMenuOpen(false)}
-          className="font-heading text-xl font-bold bg-linear-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent hover:opacity-80 transition-opacity"
+          className="flex items-center gap-2 hover:opacity-80 transition-opacity"
         >
-          Game Nights
+          {/* eslint-disable-next-line @next/next/no-img-element */}
+          <img src="/logo.png" alt="Game Nights" className="w-7 h-7 mr-1" />
+          <span className="font-heading text-xl font-bold bg-linear-to-r from-purple-400 to-blue-400 bg-clip-text text-transparent">
+            Game Nights
+          </span>
         </Link>
 
         {/* Desktop Navigation - Centered */}
